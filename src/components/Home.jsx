c2b4fe4d9055928ab352f5b1e520bee3156e64cc
elvisscochito <elvirodominguezsoriano@outlook.com>
2024-06-05 11:44:14 -0600

    style: :lipstick: Improve styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,11 @@ const Home = () => {
         <h1 className={styles.h1}>Welcome to my website!</h1>
       </header>
       <div className={styles.content}>
-        <p>Hi!, I'm <span className={styles.span}>@elvisscochito</span>, a <span className={styles.span}>Computer Science</span><br /> student at <a href="https://tec.mx/en" target="_blank" className={styles.link}>Tecnologico de Monterrey&#8599;</a> University.</p>
+        <p>Hi!, I'm <span className={styles.span}>@elvisscochito</span>, a <span className={styles.span}>Computer Science</span> student at <a href="https://tec.mx/en" target="_blank" className={styles.link}>Tecnologico de Monterrey&#8599;</a> University.</p>
         <a href="#contact" className={styles.btn}>Get in touch</a>
       </div>
+      <footer>
+      </footer>
     </section>
   );
 }
