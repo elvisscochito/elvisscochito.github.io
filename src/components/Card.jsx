aa8f1c6316ee3700e17a04161ded18099b7c9b76
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-11-04 20:52:57 -0600

    cards updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faCircleInfo, faCode } from '@fortawesome/free-solid-svg-icons';
+import { faCircleInfo, faCircleXmark, faCode } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 /* import PropTypes from 'prop-types'; */
 import { useState } from 'react';
@@ -7,47 +7,80 @@ import styles from '../styles/Card.module.css';
 
 const Card = ({ brand, project, role, date, thumbnail, logo, description, skills, /* link, */ preview, repo }) => {
   const [isFrontCard, setIsFrontCard] = useState(true);
+
+  const handleCardClick = () => {
+    setIsFrontCard(!isFrontCard);
+  };
+
   return (
-    <div className={styles.card}>
-      <header className={styles.headerIcons}>
-        {
-          role && (
-            <div className={styles.leftContainer}>
-              {/* <Tooltip text="Role Information"> */}
-              <span className={styles.role} data-tooltip-id="global-tooltip" data-tooltip-content={role}>| {role}</span>
-              {/* </Tooltip> */}
-            </div>
-          )
-        }
-        <div className={styles.middleContainer}>
-          <span className={styles.date}>{date}</span>
-        </div>
-        <div className={styles.rightContainer}>
-          {/*  <Tooltip text="React, CSS"> */}
-          <button className={styles.iconBtn} aria-label="Technologies Used" /* title="React, CSS" */ data-tooltip-id="global-tooltip" data-tooltip-content={skills.join(', ')}>
-            <FontAwesomeIcon icon={faCode} className={styles.codeIcon} />
-          </button>
-          {/* </Tooltip> */}
-          {/* <Tooltip text="View Source Code on GitHub Repository"> */}
-          <a href={repo} target='_blank' rel='noopener noreferrer' className={styles.link} /* title="View Source Code on GitHub Repository" */ data-tooltip-id="global-tooltip" data-tooltip-content="View Source Code on GitHub">
-            <FontAwesomeIcon icon={faGithub} className={styles.gitHubIcon} />
-          </a>
-          {/*  </Tooltip> */}
-          {/* <Tooltip text="View Project Description Information"> */}
-          <button className={styles.iconBtn} aria-label="Info" /* title="View Project Description Information" */ data-tooltip-id="global-tooltip" data-tooltip-content="View Project Description Information">
-            <FontAwesomeIcon icon={faCircleInfo} className={styles.infoDescriptionIcon} />
-          </button>
-          {/* </Tooltip> */}
-        </div>
-      </header>
-      <figure className={styles.body}>
-        <img src={thumbnail} alt="Project 1" />
-        <a href={preview} target='_blank' rel='noopener noreferrer' className={styles.linkImage}><img src={logo} alt="Brand Logo" className={styles.brandLogo} /></a>
-        <figcaption className={styles.figcaption}>&quot;{project}&quot; {/* <span> */}{/* - */}• {/* | */} {brand}{/* </span> */}</figcaption>
-      </figure>
-      <footer className={styles.footer}>
-        <a href={preview} target='_blank' rel='noopener noreferrer'>Preview Project &#8599;</a>
-      </footer>
+    <div className={styles.frontCard}>
+      {
+        isFrontCard ? (
+          <>
+            <header className={styles.headerIcons}>
+              {
+                role && (
+                  <div className={styles.leftContainer}>
+                    {/* <Tooltip text="Role Information"> */}
+                    <span className={styles.role} data-tooltip-id="global-tooltip" data-tooltip-content={role}>| {role}</span>
+                    {/* </Tooltip> */}
+                  </div>
+                )
+              }
+              <div className={styles.middleContainer}>
+                <span className={styles.date}>{date}</span>
+              </div>
+              <div className={styles.rightContainer}>
+                {/*  <Tooltip text="React, CSS"> */}
+                <button className={styles.iconBtn} aria-label="Technologies Used" /* title="React, CSS" */ data-tooltip-id="global-tooltip" data-tooltip-content={`Skills/Stack: ${skills.join(', ')}`}>
+                  <FontAwesomeIcon icon={faCode} className={styles.codeIcon} />
+                </button>
+                {/* </Tooltip> */}
+                {/* <Tooltip text="View Source Code on GitHub Repository"> */}
+                <a href={repo} target='_blank' rel='noopener noreferrer' className={styles.link} /* title="View Source Code on GitHub Repository" */ data-tooltip-id="global-tooltip" data-tooltip-content="View Source Code on GitHub Repository">
+                  <FontAwesomeIcon icon={faGithub} className={styles.gitHubIcon} />
+                </a>
+                {/*  </Tooltip> */}
+                {/* <Tooltip text="View Project Description Information"> */}
+                <button className={styles.iconBtn} aria-label="Info" /* title="View Project Description Information" */ data-tooltip-id="global-tooltip" data-tooltip-content="View Project Description Information">
+                  <FontAwesomeIcon icon={faCircleInfo} className={styles.infoDescriptionIcon} onClick={handleCardClick} />
+                </button>
+                {/* </Tooltip> */}
+              </div>
+            </header>
+
+            <figure className={styles.body}>
+              <img src={thumbnail} alt="Project 1" />
+              <a href={preview} target='_blank' rel='noopener noreferrer' className={styles.linkImage}><img src={logo} alt="Brand Logo" className={styles.brandLogo} /></a>
+              <figcaption className={styles.figcaption}>&quot;{project}&quot; {/* <span> */}{/* - */}• {/* | */} {brand}{/* </span> */}</figcaption>
+            </figure>
+
+            <footer className={styles.footer}>
+              <a href={preview} target='_blank' rel='noopener noreferrer'>Preview Project &#8599;</a>
+            </footer>
+          </>
+        ) : (
+          <div className={styles.backCard}>
+            <header className={styles.headerBackIcons}>
+              <div className={styles.rightContainer}>
+                <button className={styles.iconBtn} aria-label="Info" /* title="View Project Description Information" */ data-tooltip-id="global-tooltip" data-tooltip-content="Close Project Description Information">
+                  <FontAwesomeIcon icon={faCircleXmark} className={styles.infoDescriptionIcon} onClick={handleCardClick} />
+                </button>
+                {/* </Tooltip> */}
+              </div>
+            </header>
+
+            <figure className={styles.bodyBack}>
+              <img src={thumbnail} alt="Project 1" />
+              <figcaption className={styles.description}>{description}</figcaption>
+            </figure>
+
+            <footer className={styles.footer}>
+              <a href={preview} target='_blank' rel='noopener noreferrer'>Preview Project &#8599;</a>
+            </footer>
+          </div>
+        )
+      }
     </div>
   );
 }
