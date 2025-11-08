4f74da6d80da0c973b50441025114e02dcddd4ea
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-11-08 14:21:04 -0600

    modified proxy

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -54,7 +54,7 @@ const Contact = () => {
 
       const postEmail = async () => {
         try {
-          const response = await fetch("/api/sendEmail", {
+          const response = await fetch("http://localhost:3000/api/sendEmail", {
             method: "POST",
             headers: {
               "Content-Type": "application/json"
