b7b4097b06ca05fd24c6c39e3f3f3553232b1035
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-11-20 18:20:24 -0600

    feat: :art: Change
    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import ScrollProgressBar from './components/ScrollProgressBar'
 import Skills from './components/Skills'
 import SocialPath from './components/SocialPath'
 import SoftSkills from './components/SoftSkills'
+/* import WorkExperience from './components/WorkExperience' */
 import styles from './styles/Tooltip.module.css'
 
 function App() {
