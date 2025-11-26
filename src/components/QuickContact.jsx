ee78ce96a08ffa426257b00f8cefdc49d1346257
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-11-25 19:51:49 -0600

    feat: :sparkles: Added modal and implemented
    In foms and styled

--- src/components/QuickContact.jsx
+++ src/components/QuickContact.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import styles from '../styles/QuickContact.module.css';
+import Modal from './Modal';
 
 const QuickContact = () => {
+  const modalRef = useRef(null);
+
   const [formData, setFormData] = useState({
     contactInfo: ''
   });
@@ -35,14 +38,27 @@ const QuickContact = () => {
         });
 
         if (response.ok) {
-          alert('Your quick contact info has been sent successfully! I will get in touch with you soon.');
+          /* alert('Your quick contact info has been sent successfully! I will get in touch with you soon.'); */
+          modalRef.current?.open(
+            'Message sent successfully',
+            'I will reach out to you soon!'
+          );
           /* const data = await response.json();
           console.log('Success:', data); */
           setFormData({ contactInfo: '' });
+
+          /* close modal automatically after 3 seconds */
+          setTimeout(() => {
+            modalRef.current?.close();
+          }, 3000);
         }
       } catch (error) {
         console.error('Error:', error);
         alert('There was an error sending your quick contact info. Please try again later.');
+        /* modalRef.current?.open(
+          'Error',
+          'There was an error sending your quick contact info. Please try again later.'
+        ); */
       }
     }
 
@@ -62,6 +78,7 @@ const QuickContact = () => {
           <label htmlFor="quickContactInfo" className={styles.label}>Your Email or Phone Number:</label>
           <input type="text" id="quickContactInfo" name="contactInfo" className={styles.input} placeholder="Enter your email or phone number" value={formData.contactInfo} onChange={handleChange} />
         </fieldset>
+        <Modal title="Message sent successfully" message="I will reach out to you soon!" ref={modalRef} />
 
         <button type="submit" className={styles.submitButton}>Send</button>
       </form>
