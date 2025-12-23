73087ad010d3ae343b29403926703fecdbe4ead6
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-12-22 23:42:34 -0600

    feat: :sparkles: Added currency button on settings modal
    But still needs functionality

---
+++ src/components/ToggleCurrency.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import styles from '../styles/ToggleCurrency.module.css';
+
+const ToggleCurrency = () => {
+  const { t } = useTranslation("global");
+  const [currency, setCurrency] = useState('USD');
+
+  const handleToggleCurrency = () => {
+    const newCurrency = currency === 'USD' ? 'MXN' : 'USD';
+    setCurrency(newCurrency);
+    localStorage.setItem('currency', newCurrency);
+    // TODO: Add additional logic here to update prices site-wide
+  };
+
+  return (
+    <button
+      id='currency-toggle'
+      className={styles.toggleButton}
+      onClick={handleToggleCurrency}
+      aria-label="Toggle Currency"
+    >
+      {currency === 'USD' ? (
+        <>
+          {t("Settings.ToggleCurrency.mxn")}
+          &nbsp;
+          <abbr title="Cambiar a Peso Mexicano">&#40;MXN&#41;</abbr>
+        </>
+      ) : (
+        <>
+          {t("Settings.ToggleCurrency.usd")}
+          &nbsp;
+          <abbr title="Switch to US Dollar">&#40;USD&#41;</abbr>
+        </>
+      )}
+    </button>
+  );
+};
+
+export default ToggleCurrency;
